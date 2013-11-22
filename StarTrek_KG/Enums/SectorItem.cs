ef7628eac6ee845ea342c61c0ea7cd1456a2f8a3
kevin@kevingabbert.com <kevin@kevingabbert.com@e59ddd1c-2017-4800-774e-f965a426325f>
2013-11-22 18:03:08 +0000

    Notes for later.

--- StarTrek_KG/Enums/SectorItem.cs
+++ StarTrek_KG/Enums/SectorItem.cs
@@ -1,6 +1,7 @@
 ﻿
 namespace StarTrek_KG.Enums
 {
+    //TODO: get rid of enum. A SectorItem needs to be the actual object held.  (a star, or ship (I want a starbase to be a type of ship so it can have hitpoints, shield, be hostile, and fight back.))
     public enum SectorItem
     {
         Empty,
