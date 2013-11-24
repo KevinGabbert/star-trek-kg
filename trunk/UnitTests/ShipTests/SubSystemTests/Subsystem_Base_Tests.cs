408b90ff9d2ed1c9dc3e81ddaa2ec69d1aaf95e7
kevin@kevingabbert.com <kevin@kevingabbert.com@e59ddd1c-2017-4800-774e-f965a426325f>
2013-11-24 20:40:47 +0000

    added missing files.

---
+++ trunk/UnitTests/ShipTests/SubSystemTests/Subsystem_Base_Tests.cs
@@ -0,0 +1,22 @@
+﻿using Moq;
+using NUnit.Framework;
+using StarTrek_KG.Interfaces;
+
+namespace UnitTests.ShipTests.SubSystemTests
+{
+    class Subsystem_Base_Tests
+    {
+        [Test]
+        public void TakeDamage()
+        {
+            var mockedShields = new Mock<ISubsystem>();
+
+            int i = 0;
+            int i1 = i;
+            mockedShields.Setup(c => c.GetNext(It.IsAny<int>())).Returns(() => i1).Callback(() => i = -1);
+
+            mockedShields.Object.TakeDamage();
+            Assert.Greater(1, mockedShields.Object.Damage);
+        }
+    }
+}
