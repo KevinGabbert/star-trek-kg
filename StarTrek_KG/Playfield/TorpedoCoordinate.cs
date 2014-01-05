bf278d75234f656b208d998ef4fcffe27c5b2b67
kevin@kevingabbert.com <kevin@kevingabbert.com@e59ddd1c-2017-4800-774e-f965a426325f>
2014-01-05 10:47:30 +0000

    Some refactor of torpedo functionality, renaming some of the variables. Created an object that formalizes the concept that torpedo direction is computed differently than the sector map. (those 2 coordinate systems meet right where that Math.Round() squishes them together -- not my doing!)
    I'm thinking I should eventually redo how torpedoes are fired, but this existing system actually works, albeit slightly imperfectly..  It causes the torpedoes to have a wide "blast effect" at times.. which.. probably is appropriate.. we *are* dealing with antimatter here  :D

---
+++ StarTrek_KG/Playfield/TorpedoCoordinate.cs
@@ -0,0 +1,51 @@
+﻿namespace StarTrek_KG.Playfield
+{
+    public class FiringCoordinate
+    {
+       #region Properties
+
+        private double _x;
+        public double X
+        {
+            get { return _x; }
+            set
+            {
+                _x = value;
+            }
+        }
+
+        private double _y;
+
+        public double Y
+        {
+            get { return _y; }
+            set
+            {
+                _y = value;
+            }
+        }
+
+        #endregion
+
+        public FiringCoordinate()
+        {
+        }
+
+        public FiringCoordinate(Coordinate xy)
+        {
+            this.X = xy.X;
+            this.Y = xy.Y;
+        }
+
+        public FiringCoordinate(double x, double y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+
+        public override string ToString()
+        {
+            return "Coordinate: " + this.X + ", " + this.Y;
+        }
+    }
+}
