3b1e7cc0ffe811f299b7e1acedb50f4e7b2ded78
kevin@kevingabbert.com <kevin@kevingabbert.com@e59ddd1c-2017-4800-774e-f965a426325f>
2014-01-13 09:28:06 +0000

    Threw down some code that gives some ways to make music from the PC speaker..

---
+++ UnitTests/UtilityTests/MelodyPlayer_Tests.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+using StarTrek_KG.Utility;
+
+namespace UnitTests.UtilityTests
+{
+    public class MelodyPlayer_Tests
+    {
+        [Test]
+        public void test()
+        {
+            var m = new MelodyPlayer();
+
+            m.Main();
+        }
+    }
+}
