aa722d65ff9145c8e9e451817584a097963adad1
kevin@kevingabbert.com <kevin@kevingabbert.com@e59ddd1c-2017-4800-774e-f965a426325f>
2014-02-04 08:04:20 +0000

    Threats from factions are now supported in the config file.

---
+++ StarTrek_KG/Config/Collections/SeverityValues.cs
@@ -0,0 +1,52 @@
+﻿using System.Configuration;
+using E = StarTrek_KG.Config.Elements;
+
+namespace StarTrek_KG.Config.Collections
+{
+    public class SeverityValues : ConfigurationElementCollection
+    {
+        public E.SeverityValueElement this[int index]
+        {
+            get
+            {
+                return base.BaseGet(index) as E.SeverityValueElement;
+            }
+            set
+            {
+                if (base.BaseGet(index) != null)
+                {
+                    base.BaseRemoveAt(index);
+                }
+
+                this.BaseAdd(index, value);
+            }
+        }
+
+        public new E.SeverityValueElement this[string responseString]
+        {
+            get
+            {
+                return (E.SeverityValueElement)BaseGet(responseString);
+            }
+            set
+            {
+                if (BaseGet(responseString) != null)
+                {
+                    BaseRemoveAt(BaseIndexOf(BaseGet(responseString)));
+                }
+
+                this.BaseAdd(value);
+            }
+        }
+
+        protected override ConfigurationElement CreateNewElement()
+        {
+            return new E.SeverityValueElement();
+        }
+
+        protected override object GetElementKey(ConfigurationElement element)
+        {
+            return ((E.SeverityValueElement)element).value;
+        }
+    }
+}
