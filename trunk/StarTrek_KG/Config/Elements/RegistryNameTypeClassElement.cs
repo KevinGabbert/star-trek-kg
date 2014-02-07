2f17157e91bb651b2f0a746070cfee30074c600c
kevin@kevingabbert.com <kevin@kevingabbert.com@e59ddd1c-2017-4800-774e-f965a426325f>
2014-02-07 06:54:05 +0000

    done with app.config ship cleanup. duplicate ship names removed added config element classes to handle new ship attributes. everything works again.

---
+++ trunk/StarTrek_KG/Config/Elements/RegistryNameTypeClassElement.cs
@@ -0,0 +1,43 @@
+﻿using System.Configuration;
+
+namespace StarTrek_KG.Config.Elements
+{
+    public class RegistryNameTypeClassElement : ConfigurationElement
+    {
+        [ConfigurationProperty("name", IsRequired = true)]
+        public string name
+        {
+            get
+            {
+                return this["name"] as string;
+            }
+        }
+
+        [ConfigurationProperty("registry", IsRequired = false)]
+        public string registry
+        {
+            get
+            {
+                return this["registry"] as string;
+            }
+        }
+
+        [ConfigurationProperty("type", IsRequired = false)]
+        public string type
+        {
+            get
+            {
+                return this["type"] as string;
+            }
+        }
+
+        [ConfigurationProperty("class", IsRequired = false)]
+        public string @class
+        {
+            get
+            {
+                return this["class"] as string;
+            }
+        }
+    }
+}
